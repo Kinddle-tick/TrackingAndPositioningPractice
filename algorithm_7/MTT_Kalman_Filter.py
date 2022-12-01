d5fd7cec7aa4e7cbf75e8d31357afdb967da2d9e
kinddlelee <h88888888123@163.com>
2022-12-01 21:49:54 +0800

    [+]MTT单站多目标跟踪 [+]MTT多站多目标跟踪 [+]MTT轨迹关联-单站NN分类 [+]MTT卡尔曼多目标跟踪方法

--- algorithm_7/MTT_Kalman_Filter.py
+++ algorithm_7/MTT_Kalman_Filter.py
@@ -29,8 +29,8 @@ H = np.array([[1, 0, 0, 0],
 # Q = np.diag([0.02, 0.001])
 Q = np.array([np.diag([1e-4, 1e-4]), np.diag([9e-4, 9e-4])])  # 目标二更不稳定一些
 R = np.array([np.diag([.25, .25]), np.diag([.81, .81])])  # 观测站1更精确一些
-
-np.random.seed(1)  # 固定随机种子
+print("seed:", np.random.get_state()[1][0])
+np.random.seed(1085424119)  # 固定随机种子
 # 提前计算噪声
 W = np.zeros([2, TargetNum, T])
 V = np.zeros([2, TargetNum, StationNum, T])
@@ -67,11 +67,11 @@ def KalmanFilter(Xin, Zin, Pin, F, G, H, Q, R):
     Zpre = H.dot(Xpre)
     Ppre = F.dot(Pin).dot(F.T) + G.dot(Q).dot(G.T)
 
-    K = Ppre.dot(H.T).dot(np.linalg.inv(H.dot(Ppre).dot(H.T)+R))
+    K = Ppre.dot(H.T).dot(np.linalg.inv(H.dot(Ppre).dot(H.T) + R))
     e = Zin - Zpre
     Xout = Xpre + K.dot(e)
-    Pout = (np.eye(4)-K.dot(H)).dot(Ppre)
-    return Xout,Pout
+    Pout = (np.eye(4) - K.dot(H)).dot(Ppre)
+    return Xout, Pout
 
 
 if __name__ == '__main__':
@@ -89,9 +89,9 @@ if __name__ == '__main__':
     X_pre_mean[:, :, 0] = np.average(Xkf[:, :, :, 0], axis=2)
 
     Z = np.zeros([2, TargetNum, StationNum, T])  # 观测
-    Z[:, :, :, 0] = H.dot(X[:, :, 0]) + V[:, :, :, 0]
+    Z[:, :, :, 0] = H.dot(X[:, :, 0])[:, :, None] + V[:, :, :, 0]
     Zun = np.empty([2, TargetNum, StationNum, T])  # 近邻算法排序后的观测
-    Zun[:] = Z
+    Zun[:] = Z[:]
 
     for t in range(1, T):
         # ZZ = np.zeros([2,TargetNum,StationNum,T])
@@ -106,12 +106,72 @@ if __name__ == '__main__':
         # Kalman滤波
         for i in range(TargetNum):
             for j in range(StationNum):
-                Xkf[:,i,j,t],P[i,j] = KalmanFilter(Xkf[:,i,j,t-1],Zun[:,i,j,t],P[i,j],F,G,H,Q[i],R[j])
+                Xkf[:, i, j, t], P[i, j] = KalmanFilter(Xkf[:, i, j, t - 1], Zun[:, i, j, t], P[i, j], F, G, H, Q[i],
+                                                        R[j])
 
         # 融合结果
         X_pre_mean[:, :, t] = np.average(Xkf[:, :, :, t], axis=2)
 
-    # 误差分析时间
+    fig = plt.figure(figsize=[10,5*(StationNum//2+1)])
+    # 画图
+    for j in range(StationNum):
+        plt.subplot(StationNum//2+1,2,j+1)
+        plt.xlabel("X/m")
+        plt.ylabel("Y/m")
+        plt.title(f"Station_{j}")
+        # 真实轨迹 观测数据 关联结果 滤波结果
+        for i in range(TargetNum):
+            plt.plot(X[0, i, :], X[2, i, :], label=f"真实轨迹_{i}", linestyle="-")
+        for i in range(TargetNum):
+            plt.plot(Xkf[0, i, j, :], Xkf[2, i, j, :], label=f"滤波后轨迹_{i}", linestyle="-.")
+            plt.scatter(Xkf[0, i, j, :], Xkf[2, i, j, :], marker="+")
+        for i in range(TargetNum):
+            plt.scatter(Zun[0, i, j, :], Zun[1, i, j, :], label=f"观测数据_{i}", marker="s")
+        # for i in range(TargetNum):
+        #     plt.scatter(Z[0,i,j,:],Z[1,i,j,:],label=f"观测原始数据_{i}",marker="s")
+
+        plt.legend()
+        # plt.show()
+
+    # fig = plt.figure()
+    plt.subplot(StationNum//2+1,2,StationNum+1)
+    plt.xlabel("X/m")
+    plt.ylabel("Y/m")
+    # 真实轨迹 观测数据 关联结果 滤波结果
+    for i in range(TargetNum):
+        plt.plot(X[0, i, :], X[2, i, :], label=f"真实轨迹_{i}", linestyle="-")
+        plt.plot(X_pre_mean[0, i, :], X_pre_mean[2, i, :], label=f"融合轨迹_{i}", linestyle="-")
+    # for i in range(TargetNum):
+    #     plt.scatter(Z[0,i,j,:],Z[1,i,j,:],label=f"观测原始数据_{i}",marker="s")
+
+    plt.legend()
+    # plt.show()
+
+    # 误差分析
+
+    # Div_Observation = np.zeros([1, TargetNum, StationNum, T])
+    # Div_Fusion = np.zeros([1, TargetNum, T])
+    # Xreal = X[[0, 2], :, :]
+    Div_Observation = np.sqrt(np.sum((X[[0,2], :, None, :] - Zun) ** 2, axis=0))
+    Div_Fusion = np.sqrt(np.sum((X[[0,2], :, :] - X_pre_mean[[0,2], :, :]) ** 2, axis=0))
+    Div_Observation_mean = np.average(Div_Observation,axis=-1)
+    Div_Fusion_mean = np.average(Div_Fusion, axis=-1)
+    for i in range(TargetNum):
+        for j in range(StationNum):
+            print(f"观测站{j}对目标{i}的平均观测误差为：{Div_Observation_mean[i,j]}")
+        print(f"对目标{i}的融合观测误差为：{Div_Fusion_mean[i]}")
+        print("----------------------------")
+
+    # 每个站点的平均偏差
+    # plt.figure()
+    plt.subplot(StationNum//2+1,2,StationNum+2)
+    x= [0,1,2]
+    color = ['pink', 'blue', 'green', 'orchid', 'deepskyblue']
+    x_label = ['站点1偏差', '站点2偏差', '融合偏差']
+    plt.xticks(x, x_label)  # 绘制x刻度标签
+    plt.bar(x,np.average(np.c_[Div_Observation_mean,Div_Fusion_mean],axis=0),color=color[:3])
+    plt.ylabel("平均误差")
+    plt.show()
 
 
 
